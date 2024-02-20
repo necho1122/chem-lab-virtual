682c4167f13b2308308ac4bf8c4ddd0b4337f234
Nelson <nelsonubac90@gmail.com>
2024-02-20 15:56:08 -0300

    Fix navbar dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,42 @@
 import "../assets/styles/NavBar.css";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Bars, CloseWindows } from "./Icons";
 import logo from "../assets/images/chemical-icon.svg";
 
 function NavBar() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [isDropdownExpOpen, setIsDropdownExpOpen] = useState(false);
+	const dropdownRef = useRef(null);
 
 	const handleClick = () => {
 		setIsMenuOpen(!isMenuOpen);
 	};
 
+	const handleDropdownExp = () => {
+		setIsDropdownExpOpen(!isDropdownExpOpen);
+	};
+
+	const handleClickOutside = (event) => {
+		if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+			setIsDropdownExpOpen(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [handleClickOutside]);
+
 	return (
 		<nav className="navbar">
 			<div className="responsive-navbar">
 				<div className="chemical-logo">
-					<img src={logo} width={40} height={40} alt="chemical logo" />
+					<a href="/">
+						<img src={logo} width={40} height={40} alt="chemical logo" />
+					</a>
 					<h2>ChemLab</h2>
 				</div>
 				<div
@@ -35,18 +56,26 @@ function NavBar() {
 					<Link to="/about">Acerca de</Link>
 				</li>
 				<li className="dropdown-container">
-					<a href="/services">Experimentos</a>
-					<div className="dropdown">
-						<Link to="/titration" className="dpdw-one">
-							Titulación
-						</Link>
-						<Link to="/balance" className="dpdw-one">
-							Balance de masas
-						</Link>
-						<Link to="/calc" className="dpdw-one">
-							Cálculos
-						</Link>
-					</div>
+					<button
+						type="button"
+						className="experiments-dropdown"
+						onClick={handleDropdownExp}
+					>
+						Experimentos
+					</button>
+					{isDropdownExpOpen && (
+						<div className="dropdown" ref={dropdownRef}>
+							<Link to="/titration" className="dpdw-one">
+								Titulación
+							</Link>
+							<Link to="/balance" className="dpdw-one">
+								Balance de masas
+							</Link>
+							<Link to="/calc" className="dpdw-one">
+								Cálculos
+							</Link>
+						</div>
+					)}
 				</li>
 				<li>
 					<Link to="/docs">Docs</Link>
